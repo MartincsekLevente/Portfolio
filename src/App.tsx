0ca3b457eaea1016a36046b07b2accbe1d6e9280
Martincsek Levente <martincsekl@gmail.com>
2025-03-30 13:14:29 +0200

    P-3: Add Sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import './App.css'
+import './App.scss'
 import { Button } from "@mantine/core";
 
 export default function App() {
 
     return (
-        <>
+        <div className="page-container">
             <Button>test</Button>
-        </>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
     )
 }
 
