0635d3cb726fef791089e19f80f552f3ec4b7d1d
Martincsek Levente <martincsekl@gmail.com>
2025-03-30 16:18:10 +0200

    P-5: Add support for scrolling between sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import './App.scss'
 import Navbar from "./components/navbar/Navbar.tsx";
 import HomeSection from "./sections/home/HomeSection.tsx";
+import AboutSection from "./sections/about/AboutSection.tsx";
 
 export default function App() {
 
     return (
         <>
+            <div className="page-bg"></div>
             <Navbar></Navbar>
             <div className="page-container">
                 <HomeSection></HomeSection>
+                <AboutSection></AboutSection>
             </div>
         </>
     )
