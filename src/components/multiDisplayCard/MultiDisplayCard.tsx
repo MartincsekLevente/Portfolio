6d193e4163c49d69faa0079f25b0ad95c615fb83
Martincsek Levente <martincsekl@gmail.com>
2025-04-04 15:57:44 +0200

    P-12: Add Experience style and content

--- src/components/multiDisplayCard/MultiDisplayCard.tsx
+++ src/components/multiDisplayCard/MultiDisplayCard.tsx
@@ -1,17 +1,34 @@
 import './MultiDisplayCard.scss';
+import { ReactNode } from "react";
 
 interface MultiDisplayCardProps {
     title: string,
-    textList: string[],
+    subtitle: string,
+    description: string,
+    textList: ReactNode[],
 }
 
-export default function MultiDisplayCard({title, textList}: MultiDisplayCardProps) {
+export default function MultiDisplayCard({title, subtitle, description, textList}: MultiDisplayCardProps) {
     return (
         <div className="multi-display-card-container">
-            <div className="multi-display-card-title">
-                {title}
+            <div className="multi-display-card-overlay">
+                <div className="multi-display-card-title">
+                    <span className="highlight-text">{title}</span>
+                </div>
+                <div className="multi-display-card-subtitle">
+                    {subtitle}
+                </div>
+                <div className="multi-display-card-description">
+                    {description}
+                </div>
+                <div className="multi-display-card-list-container">
+                    <ul>
+                        {textList ? textList.map((text) => {
+                            return (<li className="multi-display-card-list-item">{text}</li>);
+                        }) : null}
+                    </ul>
+                </div>
             </div>
-            {textList ? textList.map(text => text) : null}
         </div>
     );
 }
--- src/sections/experience/ExperienceSection.tsx
+++ src/sections/experience/ExperienceSection.tsx
@@ -9,26 +9,53 @@ export default function ExperienceSection() {
                     Experience
                 </div>
                 <div className="experience-line-container">
-                    <svg className="experience-lines" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid meet">
-                        <line x1="50" y1="0" x2="50" y2="75" stroke="#1b2047" stroke-width="1"></line>
+                    <svg className="experience-line" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid meet">
+                        <defs>
+                            <linearGradient id="grad" x1="0" x2="0" y1="0" y2="1">
+                                <stop offset="80%" stopColor="#4a9cc6"/>
+                                <stop offset="100%" stopColor="rgba(27, 32, 71, 0.0)"/>
+                            </linearGradient>
+                        </defs>
+                        <rect id="rect1" x="49.5" y="3" width="1" height="100" fill="url('#grad')"/>
                     </svg>
+                    <div className="experience-line-point-1"></div>
+                    <div className="experience-line-point-2"></div>
                 </div>
                 <div className="multi-display-cards-container">
                     <MultiDisplayCard
-                        title="Gerhardt Informatics"
+                        title="University of Szeged"
+                        subtitle="BSc. Computer Science Degree"
+                        description="2020 - 2024"
                         textList={[
-                            "Collaborated with multiple Agile teams to develop and maintain enterprise applications for the " +
-                            "German company Brückner.",
-                            "Specialized in Frontend development, leveraging expertise in Angular and custom UI components.",
-                            "Developed custom web components in Angular with D3.js and Angular Material, improving data " +
-                            "visualization performance."]}>
+                            <>Learned the fundamentals of software development</>,
+                            <>Gained valuable experience in team collaboration through group projects</>,
+                            <>My <span className="highlight-text">Thesis</span> was about generating and solving several
+                                discrete mathematics problems</>
+                        ]}>
                     </MultiDisplayCard>
                     <MultiDisplayCard
-                        title="University of Szeged"
-                        textList={["text1", "text2", "text3"]}>
+                        title="Gerhardt Informatics"
+                        subtitle="Full-stack Software Engineer"
+                        description="2022 - 2025"
+                        textList={[
+                            <>Collaborated with multiple Agile teams to develop and maintain enterprise applications
+                                for the German company <span className="highlight-text">Brückner</span></>,
+                            <>Specialized in frontend development, developed custom web components in <span
+                                className="highlight-text">Angular</span> with <span
+                                className="highlight-text">D3.js</span> and <span className="highlight-text">Angular Material</span>,
+                                improving data visualization performance</>,
+                            <>Enhanced backend functionality by implementing <span
+                                className="highlight-text">Java</span> and <span
+                                className="highlight-text">Kotlin</span> services</>,
+                            <>Built automation tests with <span className="highlight-text">Playwright</span>,
+                                improving testing efficiency</>,
+                            <>Wrote <span className="highlight-text">Python scripts</span> for various calculation
+                                tasks</>,
+                            <>Completed an <span className="highlight-text">Agile software development training in Munich</span></>,
+                            <>Practiced <span className="highlight-text">Clean Code</span> principles</>
+                        ]}>
                     </MultiDisplayCard>
                 </div>
-
             </div>
         </section>
     );
