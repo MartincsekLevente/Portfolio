136df64acd9a0175e7c6351c0e6f84c8b8f6c7d5
Martincsek Levente <martincsekl@gmail.com>
2025-04-11 14:15:21 +0200

    P-16: Add project background images

--- src/constants/projects.tsx
+++ src/constants/projects.tsx
@@ -1,6 +1,6 @@
 import { ProjectModel } from "../models/ProjectModel.ts";
-import { FaGithub, FaJava, FaPython, FaReact, FaSass } from "react-icons/fa";
-import { SiTypescript } from "react-icons/si";
+import { FaCss3, FaReact, FaSass } from "react-icons/fa";
+import { SiMantine, SiTypescript } from "react-icons/si";
 
 export const PROJECTS: ProjectModel[] =
     [
@@ -8,26 +8,28 @@ export const PROJECTS: ProjectModel[] =
             id: "0",
             title: "My Portfolio",
             stackListIcons: [
-                <FaGithub></FaGithub>,
                 <FaReact></FaReact>,
-                <FaSass></FaSass>,
-                <SiTypescript></SiTypescript>
+                <SiTypescript></SiTypescript>,
+                <FaSass></FaSass>
             ]
         },
         {
             id: "1",
             title: "Mars Weather",
             stackListIcons: [
-                <FaGithub></FaGithub>,
-                <FaPython></FaPython>
+                <FaReact></FaReact>,
+                <SiTypescript></SiTypescript>,
+                <FaCss3></FaCss3>
             ]
         },
         {
             id: "2",
             title: "Avatar Creator",
             stackListIcons: [
-                <FaJava></FaJava>,
-                <FaReact></FaReact>
+                <FaReact></FaReact>,
+                <SiTypescript></SiTypescript>,
+                <SiMantine></SiMantine>,
+                <FaCss3></FaCss3>
             ]
         }
     ]
