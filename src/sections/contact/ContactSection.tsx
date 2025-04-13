c9bc344071bbf14876828ac03f42ff3b3d52755f
Martincsek Levente <martincsekl@gmail.com>
2025-04-13 18:43:08 +0200

    P-19: Add ContactSection and email functionality

---
+++ src/sections/contact/ContactSection.tsx
@@ -0,0 +1,27 @@
+import './ContactSection.scss';
+import { IoMdMail } from "react-icons/io";
+import { useState } from "react";
+
+export default function ContactSection() {
+
+    const [buttonHovered, setButtonHovered] = useState(false)
+
+    return (
+        <section id="contact">
+            <div className="contact-section-container">
+                <div className="contact-section-description">
+                    Got questions, project ideas, or just want to say hi? Feel free to reach out!
+                </div>
+                <div className="contact-button-container"
+                     onMouseEnter={() => setButtonHovered(true)}
+                     onMouseLeave={() => setButtonHovered(false)}>
+                    <div className={`contact-button ${buttonHovered ? 'hovered' : null}`}
+                         onClick={() => window.location.href = 'mailto:martincsekl@gmail.com'}>
+                        Send me an email!
+                        <IoMdMail></IoMdMail>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
