f34a4a2d88bbc97ab76b22b5d1a723e3f062c68d
Martincsek Levente <martincsekl@gmail.com>
2025-04-14 20:37:01 +0200

    P-22: Refine Home Section

--- src/sections/home/HomeSection.tsx
+++ src/sections/home/HomeSection.tsx
@@ -4,17 +4,19 @@ export default function HomeSection() {
     return (
         <section id="home">
             <div className="home-section-container">
+                <div className="home-section-picture-container">
+                    <img className="home-section-picture" src="src/assets/profile.jpg" alt="profile-picture"></img>
+                </div>
                 <div className="home-section-title-container">
+                    <div className="home-logo">
+                        M<span className="home-logo-right">L</span>
+                    </div>
                     <div className="home-section-title">
                         Hey, I am <span className="highlight-text">Levi</span> 👋
                     </div>
-                    <hr></hr>
                     <div className="home-section-subtitle">
                         I am a <span className="highlight-text">Frontend</span> developer from Hungary!
                     </div>
-                </div>
-                <div className="home-section-picture-container">
-                    <img className="home-section-picture" src="src/assets/profile.jpg" alt="profile-picture"></img>
                 </div>
             </div>
         </section>
