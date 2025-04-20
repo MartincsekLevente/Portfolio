20418a3f0fddd381e73385d80cae8a60547d65a6
Martincsek Levente <martincsekl@gmail.com>
2025-04-20 21:12:56 +0200

    P-38: Add motion animation to projectsSection

--- src/sections/projects/ProjectsSection.tsx
+++ src/sections/projects/ProjectsSection.tsx
@@ -3,6 +3,7 @@ import StackCard from "../../components/stackCard/StackCard.tsx";
 import { useState } from "react";
 import { PROJECTS } from "../../constants/projects.tsx";
 import DoubleLinkButton from "../../components/doubleLinkButton/DoubleLinkButton.tsx";
+import FadeInWhenVisible from "../../components/motion/fadeInWhenVisible/FadeInWhenVisible.tsx";
 
 export default function ProjectsSection() {
     const [currentProjectIndex, setCurrentProjectIndex] = useState(0);
@@ -23,40 +24,48 @@ export default function ProjectsSection() {
     return (
         <section id="projects">
             <div className="projects-section-container">
-                <div className="projects-section-title">
-                    My Projects
-                </div>
-                <div className="projects-showcase-container">
-                    <div className="projects-showcase-overlay">
-                        {PROJECTS[currentProjectIndex - 1] ?
-                            <div className="projects-showcase-left-arrow"
-                                 onClick={() => decreaseCurrentProjectIndex()}
-                            >&lt;</div> : null}
-                        {PROJECTS[currentProjectIndex + 1] ?
-                            <div className="projects-showcase-right-arrow"
-                                 onClick={() => increaseCurrentProjectIndex()}
-                            >&gt;</div> : null}
-                        <div className="project-title">
-                            {PROJECTS[currentProjectIndex].title}
-                        </div>
-                        <div className="projects-stack-container">
-                            {PROJECTS[currentProjectIndex].stackList.map((currentStack) => {
-                                return <StackCard key={crypto.randomUUID()} icon={currentStack.icon}
-                                                  name={currentStack.name}></StackCard>
-                            })}
-                        </div>
-                        <div className="project-image-container">
-                            <img className="project-image" src={PROJECTS[currentProjectIndex].bgImgUrl}
-                                 alt="missingBG"></img>
-                        </div>
-                        <div className="project-links">
-                            <DoubleLinkButton
-                                githubLink={PROJECTS[currentProjectIndex].githubUrl}
-                                websiteLink={PROJECTS[currentProjectIndex].websiteUrl}>
-                            </DoubleLinkButton>
+                <FadeInWhenVisible
+                    duration={1.5}>
+                    <div className="projects-section-title">
+                        My Projects
+                    </div>
+                </FadeInWhenVisible>
+                <FadeInWhenVisible
+                    offset={10}
+                    direction={"down"}
+                    initialDelay={0.3}>
+                    <div className="projects-showcase-container">
+                        <div className="projects-showcase-overlay">
+                            {PROJECTS[currentProjectIndex - 1] ?
+                                <div className="projects-showcase-left-arrow"
+                                     onClick={() => decreaseCurrentProjectIndex()}
+                                >&lt;</div> : null}
+                            {PROJECTS[currentProjectIndex + 1] ?
+                                <div className="projects-showcase-right-arrow"
+                                     onClick={() => increaseCurrentProjectIndex()}
+                                >&gt;</div> : null}
+                            <div className="project-title">
+                                {PROJECTS[currentProjectIndex].title}
+                            </div>
+                            <div className="projects-stack-container">
+                                {PROJECTS[currentProjectIndex].stackList.map((currentStack) => {
+                                    return <StackCard key={crypto.randomUUID()} icon={currentStack.icon}
+                                                      name={currentStack.name}></StackCard>
+                                })}
+                            </div>
+                            <div className="project-image-container">
+                                <img className="project-image" src={PROJECTS[currentProjectIndex].bgImgUrl}
+                                     alt="missingBG"></img>
+                            </div>
+                            <div className="project-links">
+                                <DoubleLinkButton
+                                    githubLink={PROJECTS[currentProjectIndex].githubUrl}
+                                    websiteLink={PROJECTS[currentProjectIndex].websiteUrl}>
+                                </DoubleLinkButton>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </FadeInWhenVisible>
             </div>
         </section>
     );
