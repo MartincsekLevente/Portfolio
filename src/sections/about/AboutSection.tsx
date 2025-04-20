4fed14dc84f2e9acfc245212223690afd20d1b42
Martincsek Levente <martincsekl@gmail.com>
2025-04-20 19:58:47 +0200

    P-34: Add motion animation to aboutSection

--- src/sections/about/AboutSection.tsx
+++ src/sections/about/AboutSection.tsx
@@ -2,48 +2,64 @@ import './AboutSection.scss';
 import CardWithIcon from "../../components/cardWithIcon/CardWithIcon.tsx";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { TbFileCv } from "react-icons/tb";
+import FadeInWhenVisible from "../../components/motion/fadeInWhenVisible/FadeInWhenVisible.tsx";
 
 export default function AboutSection() {
 
     return (
         <section id="about">
             <div className="about-section-container">
-                <div className="about-section-title">
-                    About me
-                </div>
+                <FadeInWhenVisible
+                    duration={1.5}>
+                    <div className="about-section-title">
+                        About me
+                    </div>
+                </FadeInWhenVisible>
                 <div className="about-me-content-container">
-                    <div className="about-me-card-overlay">
-                        <div className="about-me-card">
-                            <div className="about-text">
-                                As a <span
-                                className="highlight-text">frontend developer with 2+ years of experience</span>,
-                                I confidently work both in a team and
-                                independently. I
-                                enjoy working with new technologies and continuously improving my professional skills.
-                                In my
-                                free time I like to
-                                develop <span className="highlight-text">React</span> projects and create interesting
-                                web
-                                components.
+                    <FadeInWhenVisible
+                        direction={"right"}
+                        offset={15}>
+                        <div className="about-me-card-overlay">
+                            <div className="about-me-card">
+                                <div className="about-text">
+                                    As a <span
+                                    className="highlight-text">frontend developer with 2+ years of experience</span>,
+                                    I confidently work both in a team and
+                                    independently. I
+                                    enjoy working with new technologies and continuously improving my professional
+                                    skills.
+                                    In my
+                                    free time I like to
+                                    develop <span className="highlight-text">React</span> projects and create
+                                    interesting
+                                    web
+                                    components.
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </FadeInWhenVisible>
                     <div className="about-me-small-cards">
-                        <CardWithIcon
-                            icon={<FaGithub></FaGithub>}
-                            title="GitHub"
-                            url="https://github.com/MartincsekLevente">
-                        </CardWithIcon>
-                        <CardWithIcon
-                            icon={<TbFileCv></TbFileCv>}
-                            title="CV"
-                            url="/cv-martincsek-levente.pdf">
-                        </CardWithIcon>
-                        <CardWithIcon
-                            icon={<FaLinkedin></FaLinkedin>}
-                            title="LinkedIn"
-                            url="https://www.linkedin.com/in/martincseklevente/">
-                        </CardWithIcon>
+                        <FadeInWhenVisible
+                            direction={"right"}
+                            initialDelay={0.5}
+                            childDelay={0.3}
+                            offset={15}>
+                            <CardWithIcon
+                                icon={<FaGithub></FaGithub>}
+                                title="GitHub"
+                                url="https://github.com/MartincsekLevente">
+                            </CardWithIcon>
+                            <CardWithIcon
+                                icon={<TbFileCv></TbFileCv>}
+                                title="CV"
+                                url="/cv-martincsek-levente.pdf">
+                            </CardWithIcon>
+                            <CardWithIcon
+                                icon={<FaLinkedin></FaLinkedin>}
+                                title="LinkedIn"
+                                url="https://www.linkedin.com/in/martincseklevente/">
+                            </CardWithIcon>
+                        </FadeInWhenVisible>
                     </div>
                 </div>
             </div>
