be7705b0c394b7df88dd9b74df89adc77e97c1f5
Martincsek Levente <martincsekl@gmail.com>
2025-04-20 20:43:35 +0200

    P-35: Add motion animation to skillsSection

--- src/components/motion/fadeInWhenVisible/FadeInWhenVisible.tsx
+++ src/components/motion/fadeInWhenVisible/FadeInWhenVisible.tsx
@@ -3,11 +3,12 @@ import { motion } from "framer-motion";
 
 interface FadeInWhenVisibleProps {
     children: React.ReactNode | React.ReactNode[],
-    direction?: "left" | "right",
+    direction?: "left" | "right" | "up" | "down",
     offset?: number,
     duration?: number,
     initialDelay?: number,
     childDelay?: number,
+    inheritParentSize?: boolean
 }
 
 export default function FadeInWhenVisible({
@@ -16,9 +17,10 @@ export default function FadeInWhenVisible({
                                               offset = 0,
                                               duration = 0.8,
                                               initialDelay = 0,
-                                              childDelay = 0
+                                              childDelay = 0,
+                                              inheritParentSize = false
                                           }: FadeInWhenVisibleProps) {
-    const finalOffset = direction === "left" ? -offset : offset;
+    const finalOffset = direction === "left" || direction === "up" ? -offset : offset;
     const childrenArray = React.Children.toArray(children);
 
     return (
@@ -27,13 +29,22 @@ export default function FadeInWhenVisible({
                 return (
                     <motion.div
                         key={index}
-                        initial={{opacity: 0, transform: `translateX(${finalOffset}vw)`}}
-                        whileInView={{opacity: 1, transform: "translateX(0vw)"}}
+                        initial={{
+                            opacity: 0,
+                            transform: direction === "left" || direction === "right" ? `translateX(${finalOffset}vw)`
+                                : `translateY(${finalOffset}vw)`
+                        }}
+                        whileInView={{
+                            opacity: 1,
+                            transform: direction === "left" || direction === "right" ? "translateX(0vw)"
+                                : "translateY(0vw)"
+                        }}
                         transition={{
                             duration: duration,
-                            delay: childDelay === 0 ? initialDelay : childDelay * (index + 1)
+                            delay: index === 0 ? initialDelay : initialDelay + (childDelay * (index + 1))
                         }}
                         viewport={{once: true, amount: 0.1}}
+                        style={inheritParentSize ? {width: "inherit", height: "inherit"} : {}}
                     >
                         {child}
                     </motion.div>
--- src/sections/skills/SkillsSection.tsx
+++ src/sections/skills/SkillsSection.tsx
@@ -25,6 +25,7 @@ import { DiScrum } from "react-icons/di";
 import { IoLogoFirebase } from "react-icons/io5";
 import { BiLogoBlender } from "react-icons/bi";
 import { useState } from "react";
+import FadeInWhenVisible from "../../components/motion/fadeInWhenVisible/FadeInWhenVisible.tsx";
 
 export default function SkillsSection() {
 
@@ -33,130 +34,156 @@ export default function SkillsSection() {
     return (
         <section id="skills">
             <div className="skills-section-container">
-                <div className="skills-section-title-overlay"
-                     onMouseEnter={() => setParentNodeHover(true)}
-                     onMouseLeave={() => setParentNodeHover(false)}>
-                    <div className={`skills-section-title ${parentNodeHover ? 'hovered' : null}`}>
-                        My Skills
+                <FadeInWhenVisible
+                    duration={0.5}>
+                    <div className="skills-section-title-overlay"
+                         onMouseEnter={() => setParentNodeHover(true)}
+                         onMouseLeave={() => setParentNodeHover(false)}>
+                        <div className={`skills-section-title ${parentNodeHover ? 'hovered' : null}`}>
+                            My Skills
+                        </div>
                     </div>
-                </div>
+                </FadeInWhenVisible>
                 <div className="skill-tree-lines-container">
-                    <svg className="skill-tree-lines" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid meet">
-                        <line x1="12" y1="0" x2="12" y2="30" stroke="#1b2047" strokeWidth="1.5"></line>
-                        <line x1="12.8" y1="30" x2="-70" y2="30" stroke="#1b2047" strokeWidth="1.5"></line>
-                        <line x1="-70" y1="29.2" x2="-70" y2="45" stroke="#1b2047" strokeWidth="1.5"></line>
-                        <line x1="50" y1="0" x2="50" y2="100" stroke="#1b2047" strokeWidth="1.5"></line>
-                        <line x1="88" y1="0" x2="88" y2="30" stroke="#1b2047" strokeWidth="1.5"></line>
-                        <line x1="87.2" y1="30" x2="170" y2="30" stroke="#1b2047" strokeWidth="1.5"></line>
-                        <line x1="170" y1="29.2" x2="170" y2="45" stroke="#1b2047" strokeWidth="1.5"></line>
-                    </svg>
+                    <FadeInWhenVisible
+                        initialDelay={0.5}
+                        duration={1}
+                        inheritParentSize={true}>
+                        <svg className="skill-tree-lines" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid meet">
+                            <line x1="12" y1="0" x2="12" y2="30" stroke="#1b2047" strokeWidth="1.5"></line>
+                            <line x1="12.8" y1="30" x2="-70" y2="30" stroke="#1b2047" strokeWidth="1.5"></line>
+                            <line x1="-70" y1="29.2" x2="-70" y2="45" stroke="#1b2047" strokeWidth="1.5"></line>
+                            <line x1="50" y1="0" x2="50" y2="100" stroke="#1b2047" strokeWidth="1.5"></line>
+                            <line x1="88" y1="0" x2="88" y2="30" stroke="#1b2047" strokeWidth="1.5"></line>
+                            <line x1="87.2" y1="30" x2="170" y2="30" stroke="#1b2047" strokeWidth="1.5"></line>
+                            <line x1="170" y1="29.2" x2="170" y2="45" stroke="#1b2047" strokeWidth="1.5"></line>
+                        </svg>
+                    </FadeInWhenVisible>
                 </div>
                 <div className="badge-anchor-left">
                     <div className="badge-wrapper-left">
-                        <Badge
-                            text="HTML"
-                            icon={<FaHtml5></FaHtml5>}>
-                        </Badge>
-                        <Badge
-                            text="CSS"
-                            icon={<FaCss3Alt></FaCss3Alt>}>
-                        </Badge>
-                        <Badge
-                            text="JAVASCRIPT"
-                            icon={<FaJs></FaJs>}>
-                        </Badge>
-                        <Badge
-                            text="TYPESCRIPT"
-                            icon={<SiTypescript></SiTypescript>}>
-                        </Badge>
-                        <Badge
-                            text="ANGULAR"
-                            icon={<FaAngular></FaAngular>}>
-                        </Badge>
-                        <Badge
-                            text="REACT"
-                            icon={<FaReact></FaReact>}>
-                        </Badge>
-                        <Badge
-                            text="REDUX"
-                            icon={<SiRedux></SiRedux>}>
-                        </Badge>
-                        <Badge
-                            text="MANTINE"
-                            icon={<SiMantine></SiMantine>}>
-                        </Badge>
-                        <Badge
-                            text="SASS"
-                            icon={<FaSass></FaSass>}>
-                        </Badge>
+                        <FadeInWhenVisible
+                            offset={5}
+                            direction={'left'}
+                            initialDelay={0.5}
+                            childDelay={0.1}>
+                            <Badge
+                                text="HTML"
+                                icon={<FaHtml5></FaHtml5>}>
+                            </Badge>
+                            <Badge
+                                text="CSS"
+                                icon={<FaCss3Alt></FaCss3Alt>}>
+                            </Badge>
+                            <Badge
+                                text="JAVASCRIPT"
+                                icon={<FaJs></FaJs>}>
+                            </Badge>
+                            <Badge
+                                text="TYPESCRIPT"
+                                icon={<SiTypescript></SiTypescript>}>
+                            </Badge>
+                            <Badge
+                                text="ANGULAR"
+                                icon={<FaAngular></FaAngular>}>
+                            </Badge>
+                            <Badge
+                                text="REACT"
+                                icon={<FaReact></FaReact>}>
+                            </Badge>
+                            <Badge
+                                text="REDUX"
+                                icon={<SiRedux></SiRedux>}>
+                            </Badge>
+                            <Badge
+                                text="MANTINE"
+                                icon={<SiMantine></SiMantine>}>
+                            </Badge>
+                            <Badge
+                                text="SASS"
+                                icon={<FaSass></FaSass>}>
+                            </Badge>
+                        </FadeInWhenVisible>
                     </div>
                 </div>
                 <div className="badge-anchor-middle">
                     <div className="badge-wrapper-middle">
-                        <Badge
-                            text="DOCKER"
-                            icon={<FaDocker></FaDocker>}>
-                        </Badge>
-                        <Badge
-                            text="JENKINS"
-                            icon={<FaJenkins></FaJenkins>}>
-                        </Badge>
-                        <Badge
-                            text="PLAYWRIGHT"
-                            icon={<FaMasksTheater></FaMasksTheater>}>
-                        </Badge>
-                        <Badge
-                            text="GIT"
-                            icon={<FaGithub></FaGithub>}>
-                        </Badge>
-                        <Badge
-                            text="JIRA"
-                            icon={<SiJirasoftware></SiJirasoftware>}>
-                        </Badge>
-                        <Badge
-                            text="TRELLO"
-                            icon={<FaTrello></FaTrello>}>
-                        </Badge>
-                        <Badge
-                            text="GODOT"
-                            icon={<SiGodotengine></SiGodotengine>}>
-                        </Badge>
-                        <Badge
-                            text="UNREAL ENGINE"
-                            icon={<SiUnrealengine></SiUnrealengine>}>
-                        </Badge>
-                        <Badge
-                            text="BLENDER"
-                            icon={<BiLogoBlender></BiLogoBlender>}>
-                        </Badge>
-                        <Badge
-                            text="SCRUM"
-                            icon={<DiScrum></DiScrum>}>
-                        </Badge>
+                        <FadeInWhenVisible
+                            offset={5}
+                            direction={'down'}
+                            initialDelay={0.5}
+                            childDelay={0.1}>
+                            <Badge
+                                text="DOCKER"
+                                icon={<FaDocker></FaDocker>}>
+                            </Badge>
+                            <Badge
+                                text="JENKINS"
+                                icon={<FaJenkins></FaJenkins>}>
+                            </Badge>
+                            <Badge
+                                text="PLAYWRIGHT"
+                                icon={<FaMasksTheater></FaMasksTheater>}>
+                            </Badge>
+                            <Badge
+                                text="GIT"
+                                icon={<FaGithub></FaGithub>}>
+                            </Badge>
+                            <Badge
+                                text="JIRA"
+                                icon={<SiJirasoftware></SiJirasoftware>}>
+                            </Badge>
+                            <Badge
+                                text="TRELLO"
+                                icon={<FaTrello></FaTrello>}>
+                            </Badge>
+                            <Badge
+                                text="GODOT"
+                                icon={<SiGodotengine></SiGodotengine>}>
+                            </Badge>
+                            <Badge
+                                text="UNREAL ENGINE"
+                                icon={<SiUnrealengine></SiUnrealengine>}>
+                            </Badge>
+                            <Badge
+                                text="BLENDER"
+                                icon={<BiLogoBlender></BiLogoBlender>}>
+                            </Badge>
+                            <Badge
+                                text="SCRUM"
+                                icon={<DiScrum></DiScrum>}>
+                            </Badge>
+                        </FadeInWhenVisible>
                     </div>
                 </div>
                 <div className="badge-anchor-right">
                     <div className="badge-wrapper-right">
-                        <Badge
-                            text="JAVA"
-                            icon={<FaJava></FaJava>}>
-                        </Badge>
-                        <Badge
-                            text="NODE.JS"
-                            icon={<FaNodeJs></FaNodeJs>}>
-                        </Badge>
-                        <Badge
-                            text="KOTLIN"
-                            icon={<SiKotlin></SiKotlin>}>
-                        </Badge>
-                        <Badge
-                            text="PYTHON"
-                            icon={<FaPython></FaPython>}>
-                        </Badge>
-                        <Badge
-                            text="FIREBASE"
-                            icon={<IoLogoFirebase></IoLogoFirebase>}>
-                        </Badge>
+                        <FadeInWhenVisible
+                            offset={5}
+                            direction={'right'}
+                            initialDelay={0.5}
+                            childDelay={0.1}>
+                            <Badge
+                                text="JAVA"
+                                icon={<FaJava></FaJava>}>
+                            </Badge>
+                            <Badge
+                                text="NODE.JS"
+                                icon={<FaNodeJs></FaNodeJs>}>
+                            </Badge>
+                            <Badge
+                                text="KOTLIN"
+                                icon={<SiKotlin></SiKotlin>}>
+                            </Badge>
+                            <Badge
+                                text="PYTHON"
+                                icon={<FaPython></FaPython>}>
+                            </Badge>
+                            <Badge
+                                text="FIREBASE"
+                                icon={<IoLogoFirebase></IoLogoFirebase>}>
+                            </Badge>
+                        </FadeInWhenVisible>
                     </div>
                 </div>
             </div>
