8677ac26ac55d55085fa01c6fe5d0b5585ceda02
Martincsek Levente <martincsekl@gmail.com>
2025-04-21 19:07:28 +0200

    P-41: Test2

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import './App.scss'
 import Navbar from "./components/navbar/Navbar.tsx";
 import HomeSection from "./sections/home/HomeSection.tsx";
-import AboutSection from "./sections/about/AboutSection.tsx";
+/*import AboutSection from "./sections/about/AboutSection.tsx";
 import SkillsSection from "./sections/skills/SkillsSection.tsx";
 import ExperienceSection from "./sections/experience/ExperienceSection.tsx";
 import ProjectsSection from "./sections/projects/ProjectsSection.tsx";
-import ContactSection from "./sections/contact/ContactSection.tsx";
+import ContactSection from "./sections/contact/ContactSection.tsx";*/
 
 export default function App() {
 
