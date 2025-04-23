4c1a18d7eb27b96a8b1d7598086071bf1784e40a
Martincsek Levente <martincsekl@gmail.com>
2025-04-23 15:49:50 +0200

    P-51: work on mobile compatibility for skilssPage

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import './App.scss'
 import Navbar from "./components/navbar/Navbar.tsx";
 import HomeSection from "./sections/home/HomeSection.tsx";
 import AboutSection from "./sections/about/AboutSection.tsx";
-/*import SkillsSection from "./sections/skills/SkillsSection.tsx";
-import ExperienceSection from "./sections/experience/ExperienceSection.tsx";
+import SkillsSection from "./sections/skills/SkillsSection.tsx";
+/*import ExperienceSection from "./sections/experience/ExperienceSection.tsx";
 import ProjectsSection from "./sections/projects/ProjectsSection.tsx";
 import ContactSection from "./sections/contact/ContactSection.tsx";*/
 
@@ -12,12 +12,12 @@ export default function App() {
     return (
         <>
             <div className="page-bg"></div>
-            <Navbar></Navbar>
+            <Navbar/>
             <div className="page-container">
-                <HomeSection></HomeSection>
-                <AboutSection></AboutSection>
-                {/*<SkillsSection></SkillsSection>
-                <ExperienceSection></ExperienceSection>
+                <HomeSection/>
+                <AboutSection/>
+                <SkillsSection/>
+                {/*<ExperienceSection></ExperienceSection>
                 <ProjectsSection></ProjectsSection>
                 <ContactSection></ContactSection>*/}
             </div>
